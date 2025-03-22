3e85bffe1ab25df1056f09424e35dacbaabf00f0
Gmontes <ghmontes92@gmail.com>
2025-03-22 19:27:11 -0300

    estructura de app, install de axios , apis , bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Cryptos from './components/Cryptos'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='container-fluid'>
+        <h1 className='text-center'>Crypto Mon</h1>
+        <Cryptos/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cryptos.jsx
@@ -0,0 +1,71 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+
+const Cryptos = () => {
+    const [busqueda, setBusqueda] = useState("");
+    const [dataCryptos, setDataCryptos] = useState([]);
+
+    const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en"
+
+    useEffect(() =>{
+        const fetchCryptos = async ()=>{
+            try{
+                const response = await axios.get(URL)
+                //console.log(response.data)
+                setDataCryptos(response.data)
+            }catch{
+                console.error("Algo salio mal",error)
+            }
+        }
+
+        fetchCryptos()
+    },[])
+
+    const buscador = (e)=>{
+        setBusqueda(e.target.value)
+    }
+
+    const resultados = !busqueda ? dataCryptos : dataCryptos.filter((crypto) => crypto.name.toLowerCase().includes(busqueda.toLocaleLowerCase())) 
+
+  return (
+    <>
+        <input value={busqueda} onChange={buscador} type='text' placeholder='Buscar Crypto...' className='form-control' />
+
+        <table className='table table-dark table-hover mt-3'>
+            <thead>
+                <tr>
+                    <th>Ranking</th>
+                    <th>Nombre</th>
+                    <th>Simbolo</th>
+                    <th>Precio</th>
+                    <th>Precio 24hs</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultados.map(crypto=> (
+                    <tr key={crypto.id}>
+                        <td>#{crypto.market_cap_rank}</td>
+                        <td>{<img src={crypto.image} className="img-tamaño m-1"/>}{crypto.name}</td>
+                        <td>{crypto.symbol.toUpperCase()}</td>
+                        <td>{crypto.current_price.toFixed(2)}</td>
+                        <td>
+                            {crypto.price_change_percentage_24h < 0 ?
+                            <span className="badge bg-danger">{crypto.price_change_percentage_24h}</span> : 
+                            <span className="badge bg-success">{crypto.price_change_percentage_24h}</span>
+                            }
+                        </td>
+
+                        
+                    </tr>
+                ))}
+            </tbody>
+
+
+        </table>
+    </>
+
+  )
+}
+
+export default Cryptos
