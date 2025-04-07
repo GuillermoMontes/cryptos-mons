6d57feb25c653acaad7c74e3c3d8c2e46c12faa0
Gmontes <ghmontes92@gmail.com>
2025-04-07 19:47:54 -0300

    framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
-
-import './App.css'
-import Cryptos from './components/Cryptos'
+import "./App.css";
+import { useEffect, useState } from "react";
+import Cryptos from "./components/Cryptos";
+import { motion } from "motion/react";
 
 function App() {
-  
+  const [cargando, setCargando] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setCargando(false), 3000);
+  }, []);
 
-  return (
-    <>
-      <div className='container-fluid '>
-        <h1 className='text-center'>Crypto Mon</h1>
-        <Cryptos/>
+  const cargaText = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
+  return cargando ? (
+    <div className="d-flex flex-column justify-content-center align-items-center overflow-hidden w-100 h-100 min-vh-100 poppins-thin bg-dark">
+      <div className="position-relative">
+        <svg height="60" width="320" className="">
+          <rect className="shape" height="60" width="250" />
+        </svg>
+        <motion.p
+          variants={cargaText}
+          initial="hidden"
+          animate="visible"
+          className="text"
+        >
+          Crypto Mon 💰
+        </motion.p>
       </div>
-
-    </>
-  )
+    </div>
+  ) : (
+    <div className="container">
+      <h1 className="text-center">Crypto Mon</h1>
+      <Cryptos />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Cryptos.jsx
+++ src/components/Cryptos.jsx
@@ -1,74 +1,97 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
+import { motion } from "motion/react"
 
 const Cryptos = () => {
-    const [busqueda, setBusqueda] = useState("");
-    const [dataCryptos, setDataCryptos] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [dataCryptos, setDataCryptos] = useState([]);
 
-    const URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en"
+  const URL =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&locale=en";
 
-    useEffect(() =>{
-        const fetchCryptos = async ()=>{
-            try{
-                const response = await axios.get(URL)
-                //console.log(response.data)
-                setDataCryptos(response.data)
-            }catch{
-                console.error("Algo salio mal",error)
-            }
-        }
+  useEffect(() => {
+    const fetchCryptos = async () => {
+      try {
+        const response = await axios.get(URL);
+        //console.log(response.data)
+        setDataCryptos(response.data);
+      } catch {
+        console.error("Algo salio mal", error);
+      }
+    };
 
-        fetchCryptos()
-    },[])
+    fetchCryptos();
+  }, []);
 
-    const buscador = (e)=>{
-        setBusqueda(e.target.value)
-    }
+  const buscador = (e) => {
+    setBusqueda(e.target.value);
+  };
 
-    const resultados = !busqueda ? dataCryptos : dataCryptos.filter((crypto) => crypto.name.toLowerCase().includes(busqueda.toLocaleLowerCase())) 
+  const resultados = !busqueda
+    ? dataCryptos
+    : dataCryptos.filter((crypto) =>
+        crypto.name.toLowerCase().includes(busqueda.toLocaleLowerCase())
+      );
 
   return (
-    <>  
-        <div className="">
+    <>
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 2.0, type: "spring" }}
+      >
+        <input
+          value={busqueda}
+          onChange={buscador}
+          type="text"
+          placeholder="Buscar Crypto..."
+          className="form-control"
+        />
 
-        <input value={busqueda} onChange={buscador} type='text' placeholder='Buscar Crypto...' className='form-control' />
-
-        <table className='table table-dark table-hover mt-3'>
-            <thead>
-                <tr>
-                    <th>Ran</th>
-                    <th>Nombre</th>
-                    <th>Sim</th>
-                    <th>Precio</th>
-                    <th>P 24hs</th>
-                </tr>
-            </thead>
-            <tbody className="">
-                {resultados.map(crypto=> (
-                    <tr key={crypto.id}>
-                        <td>#{crypto.market_cap_rank}</td>
-                        <td>{<img src={crypto.image} className="img-tamaño m-1"/>}{crypto.name}</td>
-                        <td>{crypto.symbol.toUpperCase()}</td>
-                        <td>{crypto.current_price.toFixed(2)}</td>
-                        <td>
-                            {crypto.price_change_percentage_24h < 0 ?
-                            <span className="badge bg-danger">{crypto.price_change_percentage_24h}</span> : 
-                            <span className="badge bg-success">{crypto.price_change_percentage_24h}</span>
-                            }
-                        </td>
-
-                        
-                    </tr>
-                ))}
-            </tbody>
-
-
+        <table className="table table-dark table-hover mt-3">
+          <thead>
+            <tr>
+              <th>Ran</th>
+              <th>Nombre</th>
+              <th>Sim</th>
+              <th>Precio</th>
+              <th>P 24hs</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {resultados.map((crypto) => (
+              <tr key={crypto.id}>
+                <td>#{crypto.market_cap_rank}</td>
+                <td>
+                  {<img src={crypto.image} className="img-tamaño m-1" />}
+                  {crypto.name}
+                </td>
+                <td>{crypto.symbol.toUpperCase()}</td>
+                <td>{crypto.current_price.toFixed(2)}</td>
+                <td>
+                  {crypto.price_change_percentage_24h < 0 ? (
+                    <span className="badge bg-danger">
+                      {crypto.price_change_percentage_24h}
+                    </span>
+                  ) : (
+                    <span className="badge bg-success">
+                      {crypto.price_change_percentage_24h}
+                    </span>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
-        </div>
+      </motion.div>
     </>
+  );
+};
 
-  )
-}
-
-export default Cryptos
+export default Cryptos;
